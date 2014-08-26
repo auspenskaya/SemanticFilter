ca08c57ef47e12312b8257c188009575bcb079c7
auspenskaya <ania.quies@gmail.com>
2014-08-26 17:28:34 +0400

    pause в run

--- run.cmd
+++ run.cmd
@@ -1,1 +1,1 @@
-java  -Dfile.encoding=UTF-8 -classpath "lib\SemanticFilter.jar" team112.gate.BatchProcessApp -g application.xgapp TestCorpus\*.json
+java  -Dfile.encoding=UTF-8 -classpath "lib\SemanticFilter.jar" team112.gate.BatchProcessApp -g application.xgapp TestCorpus\*.json pause
