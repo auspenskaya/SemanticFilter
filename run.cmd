7b5077f41f0febf9ea1255dd7dad54b51c08898a
auspenskaya <ania.quies@gmail.com>
2014-10-08 17:33:10 +0400

    * и + заменены, правила для дтп разбиты на отдельные фазы

--- run.cmd
+++ run.cmd
@@ -1,1 +1,2 @@
-java  -Dfile.encoding=UTF-8 -classpath "lib\SemanticFilter.jar" team112.gate.BatchProcessApp -g application.xgapp TestCorpus\*.json pause
+java -Xmx4G -Xss1G -Dfile.encoding=UTF-8 -classpath "lib\SemanticFilter.jar" team112.gate.BatchProcessApp -g application.xgapp TestCorpus\*.json 
+pause
